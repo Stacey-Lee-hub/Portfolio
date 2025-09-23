3b3d7542ee2ba74a2cbba16531cf3ba0a51139f3
Stacey-Lee-hub <staceypietersen2@gmail.com>
2025-09-23 16:38:04 +0200

    card

---
+++ src/card/card.jsx
@@ -0,0 +1,15 @@
+import './card.css'
+
+function Card() {
+    return(
+        <>
+            <div className="card-container">
+                <div className="card-item">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
