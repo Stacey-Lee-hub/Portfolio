521eee58ebc15c743cb6b8550024435cae87d9d7
Stacey-Lee-hub <staceypietersen2@gmail.com>
2025-09-23 16:47:50 +0200

    info setup

--- src/card/card.jsx
+++ src/card/card.jsx
@@ -4,9 +4,34 @@ function Card() {
     return(
         <>
             <div className="card-container">
-                <div className="card-item">
-                    
+                <div className="card">
+                    <div className="left">
+                        {/* QR CODE */}
+                        <img src="" alt="" />
+                    </div>
+                    <div className="right">
+                        <h1>Stacey-Lee Pietersen</h1>
+                        <div className="info">
+                            {/* possibly grid */}
+                            <div className="grid-left">
+                                <div className="small">Date of Birth</div>
+                                <div className="grid-info">2 JUL 2003</div>
+                                <div className="small">Pronouns</div>
+                                {/* dont stress. youre gonna die soon be unapologetic */}
+                                <div className="grid-info">They/Them</div>
+                            </div>
+                            <div className="grid-right">
+                                <div className="small">Date of Birth</div>
+                                <div className="grid-info">2 JUL 2003</div>
+                                <div className="small">Pronouns</div>
+                                {/* dont stress. youre gonna die soon be unapologetic */}
+                                <div className="grid-info">They/Them</div>
+                                {/* for reference */}
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                {/* make footer that underlines the entire card */}
             </div>
         </>
     )
--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -2,7 +2,9 @@ import './skills.css'
 
 function Skills() {
     return (
-        <></>
+        <>
+            {/* cards disperse but on ddouble click they come back together */}
+        </>
     )
 }
 
