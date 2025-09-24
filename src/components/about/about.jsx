95b16ed800210c27e9178968c65f49f708f14dce
Stacey-Lee-hub <staceypietersen2@gmail.com>
2025-09-24 17:23:10 +0200

    about setup

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,8 +1,20 @@
 import './about.css'
+import pic1 from '../../assets/profile pic/pic_1.png'
 
 function About(){
     return (
-        <></>
+        <>
+            <div className="container">
+                <div className="left">
+                    <div className="slider">
+                        <img src={pic1} alt="" />
+                    </div>
+                </div>
+                <div className="right">
+                    <h2>let me see this please</h2>
+                </div>
+            </div>
+        </>
     )
 }
 
